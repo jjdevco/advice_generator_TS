f49daa09c22b781426e70d3af7c00d703b03c1c5
Jose Jimenez <email@jimenezjose.com>
2022-05-12 12:35:02 -0500

    add tailwind css lib

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Advice Generator</h1>
+        <h1 className="text-3xl font-bold underline">Advice Generator</h1>
       </header>
     </div>
   );
